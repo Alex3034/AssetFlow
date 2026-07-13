6406ab52af758c029dfae989e26a7736dd10073b
Alejandro <alex1348wk@gmail.com>
2026-07-13 17:50:56 +0200

    Add django-crispy-forms and crispy-bootstrap5
    Set up django-crispy-forms with crispy-bootstrap5 template pack for form rendering with Bootstrap 5 styling. Includes package installation in requirements.txt and Django settings configuration.

--- AssetFlow/settings.py
+++ AssetFlow/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
 
     'django_extensions',
 
+    'crispy_forms',
+    'crispy_bootstrap5',
+
     'assets',
 ]
 
@@ -125,3 +128,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# django-crispy-forms
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+CRISPY_ALLOWED_TEMPLATE_PACKS = ['bootstrap5']
