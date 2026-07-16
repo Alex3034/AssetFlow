f9db43391fe8fcf3cb2db1bbf03b83b286a80030
Alejandro <alex1348wk@gmail.com>
2026-07-16 20:02:02 +0200

    Add user registration feature
    Implement user registration with a new RegisterView class-based view, CustomUserCreationForm for form customization, and register.html template. Added navigation link from login page to registration page.

--- assets/views.py
+++ assets/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from assets.forms import CustomUserCreationForm
+
+
+class RegisterView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('login')
+
